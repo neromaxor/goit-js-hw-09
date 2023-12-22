2ab786f044fe5950cf886fda9dfe6349eb5353a7
neromaxor <141302051+neromaxor@users.noreply.github.com>
2023-12-22 14:31:49 +0000

    Deploying to gh-pages from @ neromaxor/goit-js-hw-09@91dc57052c7d446e34958e46108f36dd8b55ccd3 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,12 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";const m=document.querySelector("section"),n=`<form class="feedback-form" autocomplete="off">
+  <label>
+    Email
+    <input type="email" name="email" autofocus />
+  </label>
+  <label>
+    Message
+    <textarea name="message" rows="8"></textarea>
+  </label>
+  <button type="submit">Submit</button>
+</form>`;m.insertAdjacentHTML("beforeend",n);const t=document.querySelector(".feedback-form"),a=t.querySelector('[name="email"]'),o=t.querySelector('[name="message"]'),r=localStorage.getItem("feedback-form-state");if(r)try{const e=JSON.parse(r);a.value=e.email,o.value=e.message}catch(e){console.error("LOCAL STORAGE PARSE ERROR",e)}t.addEventListener("input",()=>{const e={email:a.value.trim(),massage:o.value.trim()};localStorage.setItem("feedback-form-state",JSON.stringify(e))});t.addEventListener("submit",i);function i(e){e.preventDefault();const l=a.value,s=o.value;if(l===""||s===""){alert("Please fill in both fields in the form.");return}console.log({email:l,message:s}),localStorage.removeItem("feedback-form-state"),t.reset()}
+//# sourceMappingURL=commonHelpers2.js.map
